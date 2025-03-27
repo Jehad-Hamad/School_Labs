6a711211418cf5624ac58258572d7cdb0350dc82
JehadH1 <5088836@nwpolytech.ca>
2025-03-27 09:08:40 -0600

    changed first two

--- CS2910/CS2910_Lab8/Q1.sql
+++ CS2910/CS2910_Lab8/Q1.sql
@@ -1,3 +1,9 @@
+--shows all borrowers and what books they borrowed even if noneS
+select *
+from borrower as b
+LEFT JOIN book_loans as bl ON bl.card_no = b.card_no;
+
+--shows only person that has no books taken out
 SELECT *
 FROM borrower b
 WHERE NOT EXISTS (
--- CS2910/CS2910_Lab8/Q2.1.sql
+++ CS2910/CS2910_Lab8/Q2.1.sql
@@ -1,21 +1,35 @@
--- SQLite
 
+--shows books copies and what book they are as long as branch
+SELECT c.branch_id, b.title, c.no_of_copies
+FROM book_copies AS c
+JOIN book AS b ON b.book_id = c.book_id;
 
+--shows max # of books for branch 1
+SELECT c.branch_id, b.title, Max(c.no_of_copies)
+FROM book_copies AS c
+JOIN book AS b ON b.book_id = c.book_id
+WHERE c.branch_id = 1;
 
-
-
-
-
-
-
---using joins and nested
-/* SELECT b.title, c.no_of_copies
-FROM book as b
-JOIN book_copies as c on b.book_id = c.book_id
-WHERE c.no_of_copies > (
-    SELECT MAX(c2.no_of_copies)
-    FROM book_copies as c2
-    JOIN library_branch as lb ON c2.branch_id = lb.branch_id
-    where lb.branch_id = 1
+--only nested queries
+--get the branch id title and # of books where i have more books that the max amount in branch 1
+SELECT c.branch_id, b.title, c.no_of_copies
+FROM book AS b, book_copies AS c
+    WHERE b.book_id = c.book_id AND
+        c.no_of_copies > (
+        SELECT MAX(c2.no_of_copies)
+        FROM book_copies AS c2
+        WHERE c2.branch_id = 1
 );
- */
+
+
+
+
+--using joins and nested queries
+-- SELECT c.branch_id, b.title, c.no_of_copies
+-- FROM book AS b
+-- JOIN book_copies as c ON b.book_id = c.book_id
+-- WHERE c.no_of_copies > (
+--     SELECT MAX(c2.no_of_copies)
+--     FROM book_copies AS c2
+--     where c2.branch_id = 1
+-- );
