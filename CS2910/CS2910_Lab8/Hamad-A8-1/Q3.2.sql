6b557d4acf97314e2f3e81cb9005937048d1f6be
JehadH1 <5088836@nwpolytech.ca>
2025-03-27 17:08:48 -0600

    fixed

--- CS2910/CS2910_Lab8/Hamad-A8-1/Q3.2.sql
+++ CS2910/CS2910_Lab8/Hamad-A8-1/Q3.2.sql
@@ -1,21 +1,22 @@
+-- SQLite
+SELECT *
+from book_copies
+ORDER by branch_id;
 
--- Output branches that have more than 3 books
-SELECT c.branch_id, count(*)
-FROM book_copies AS c
-GROUP BY c.branch_id
+
+SELECT *, count(*)
+FROM book_copies
+GROUP BY branch_id
+HAVING COUNT(book_id) > 2;
+
+SELECT bc.branch_id, COUNT(*) AS books_c
+FROM book_copies AS bc
+WHERE bc.no_of_copies > 2
+GROUP BY branch_id;
+
+
+select branch_id, COUNT(*) AS books_c
+FROM book_copies
+WHERE no_of_copies > 2
+GROUP BY branch_id
 HAVING COUNT(*) > 2;
-
--- The total number of books whose no_copies value exceeds 5 in each branch
-SELECT c.branch_id, SUM(c.no_of_copies) AS total_copies
-FROM book_copies as c
-GROUP BY c.branch_id
-HAVING SUM(c.no_of_copies) > 5;
-
--- Q3.2
--- For each branch that has more than 3 books retrieve the branch ID 
--- and the number of those books whose no_copies value exceeds 5
-SELECT c.branch_id, SUM(c.no_of_copies) AS total_copies
-FROM book_copies as c
-GROUP BY c.branch_id
-HAVING COUNT(*) > 2 AND SUM(c.no_of_copies) > 5;
--- Needed to have more than 2 books as i love have three books max so no branch would be show up if it was 3
