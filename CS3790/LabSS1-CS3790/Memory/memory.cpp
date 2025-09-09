6eb75916e8f838490ce51a0aba344adf0c317796
Jehad Hamad <jehadads@gmail.com>
2025-09-09 16:42:06 -0600

    Refactor Memory class and update makefile for proper compilation

--- CS3790/LabSS1-CS3790/Memory/memory.cpp
+++ CS3790/LabSS1-CS3790/Memory/memory.cpp
@@ -1,1 +1,15 @@
 #include "memory.h"
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+Memory::Memory() {
+    // Initialize a 1D vector with size 10000 filled with zeros
+    memory = vector<int>(10000, 0);
+}
+
+void Memory::printMemory() {
+    cout << "MEMORY" << endl
+         << endl;
+}
