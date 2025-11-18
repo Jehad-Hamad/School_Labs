d805d2edff9f37b5bd44515112746aad9f660359
Jehad Hamad <JehadHads@gmail.com>
2025-11-18 14:35:05 -0700

    1) Now I have the walls all around 2) Changed my draw plane and init plane 3) Now I ask for a wall type and a color so I can create a differnet types of walls 4) I added a function called initObject that will take your object and than bind its buffers to the webgl buffer so you can save them and use them for later

--- CS3110/CS3110_Lab5/Task1.js
+++ CS3110/CS3110_Lab5/Task1.js
@@ -35,7 +35,7 @@ function main() {
 
     viewMatrix = new Matrix4();
     viewMatrix.setLookAt(
-        10.0, 4.0, -10.0,   
+        10, 10.5, 5,   
         0, 0, 0,        
         0, 1, 0
     );
@@ -52,49 +52,118 @@ function main() {
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    drawPlane(gl);
+    const floor = createPlane(gl, "floor", [.54, 0.60, 0.36,]);
+    // Red
+    const right = createPlane(gl, "wallRight",[1.0, 0.0, 0.0]);
+    // True Blue 0.53, 0.81, 0.92
+    const left  = createPlane(gl, "wallLeft",[0.0, 0.0, 1.0]);
+    // Yellow
+    const back  = createPlane(gl, "wallBack",[1, 1, 0.4]);
+
+    drawPlane(gl, floor);
+    drawPlane(gl, right)
+    drawPlane(gl, left)
+    drawPlane(gl, back)
+
 }
 
-function initPlane(gl) {
-    
-    const vertices = new Float32Array([
-        -7.0, 0.0, -7.0, 
-        -7.0, 0.0,  7.0, 
-        7.0, 0.0,  7.0, 
-        7.0, 0.0, -7.0,
-    ]);
+function createPlane(gl, wall, color) {
 
+    // Eight points for all walls
+    const vertex = [
+        [-7.0,  0.0, -7.0], // v0
+        [ 7.0,  0.0, -7.0], // v1
+        [ 7.0,  7.0, -7.0], // v2
+        [-7.0,  7.0, -7.0], // v3
+
+        [-7.0,  0.0,  7.0], // v4
+        [ 7.0,  0.0,  7.0], // v5
+        [ 7.0,  7.0,  7.0], // v6
+        [-7.0,  7.0,  7.0], // v7
+    ];
+
+    let vertices;
+
+    // Switch statement for different types of planes
+    switch(wall){
+
+        case "floor":
+            vertices = new Float32Array([
+            ...vertex[0],
+            ...vertex[4],
+            ...vertex[5],
+            ...vertex[1]
+        ]);
+        break;
+
+        case "wallRight":
+            vertices = new Float32Array([
+                ...vertex[1],
+                ...vertex[5],
+                ...vertex[6],
+                ...vertex[2]
+            ]);
+            break;
+
+        case "wallLeft":
+            vertices = new Float32Array([
+                ...vertex[4],
+                ...vertex[0],
+                ...vertex[3],
+                ...vertex[7]
+            ]);
+            break;
+
+        case "wallBack":
+            vertices = new Float32Array([
+                ...vertex[0],
+                ...vertex[1],
+                ...vertex[2],
+                ...vertex[3]
+            ]);
+            break;
+
+        default:
+            console.log(wall + " ISSUE")
+            vertices = new Float32Array([]);
+    }
+
+    // Unpack the color for the vertice's
     const colors = new Float32Array([
-        .54, 0.60, 0.36,
-        .54, 0.60, 0.36,
-        .54, 0.60, 0.36,
-        .54, 0.60, 0.36,
+        ...color, ...color, ...color, ...color
     ]);
 
+    // Bind and Buffer data
     return initBuffers(gl, vertices, colors, null);
 }
 
 
 function initBuffers(gl, vertices, colors, indices) {
 
+    // Create Buffers 
     const vertexBuffer = gl.createBuffer();
     const colorBuffer = gl.createBuffer();
     const indicesBuffer = gl.createBuffer();
 
+    // Bind and buffer the vertices array to the vertex buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Position);
 
+    // Bind and buffer the colors array to the color buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
     const a_Color = gl.getAttribLocation(gl.program, 'a_Color');
     gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Color);
 
-    var pointCount = vertices.length / 3;
+    // Vertex count and indices count
+    var vertexCount = vertices.length / 3;
     var indicesCount = 0
+
+    // If you have indices bind that buffer
     if (indices != null) {
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
@@ -103,10 +172,31 @@ function initBuffers(gl, vertices, colors, indices) {
     }
 
   
-    return { vertexBuffer, colorBuffer, indicesBuffer, pointCount, indicesCount, a_Position, a_Color };
+    return { vertexBuffer, colorBuffer, indicesBuffer, vertexCount, indicesCount, a_Position, a_Color };
 }
 
-function drawPlane(gl) {
-    var plane = initPlane(gl);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, plane.pointCount);
+
+
+// Function to initialize buffers for the object so I can draw later 
+function initObject(gl, object) {
+    // Bind the objects vertex buffer to the webgl buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, object.vertexBuffer);
+    gl.vertexAttribPointer(object.a_Position, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(object.a_Position);
+
+    // Bind the objects color buffer to the webgl buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, object.colorBuffer);
+    gl.vertexAttribPointer(object.a_Color, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(object.a_Color);
+
+    // Bind the objects indicesBuffer to the webgl buffer
+    if (object.indicesBuffer) {
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, object.indicesBuffer);
+    }
+}
+
+// Function to draw the flat Plane for floors and walls
+function drawPlane(gl, plane) {
+    initObject(gl, plane)
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, plane.vertexCount);
 }
