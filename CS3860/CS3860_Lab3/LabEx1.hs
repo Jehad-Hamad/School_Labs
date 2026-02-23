85b5977805f578347c74ffa4f5a3ceedbcdd2930
Jehad Hamad <JehadHads@gmail.com>
2026-02-23 14:52:36 -0700

    fix1

--- CS3860/CS3860_Lab3/LabEx1.hs
+++ CS3860/CS3860_Lab3/LabEx1.hs
@@ -4,11 +4,11 @@ CS3860
 Lab Assigment #3
 -}
 
---Task 1 
+-- Task 1
 transp :: [[Int]] -> [[Int]]
 transp [] = []
-transp ([]:_) = []
-transp xs = map head xs : transp(map tail xs)
+transp ([] : _) = []
+transp xs = map head xs : transp (map tail xs)
 
 -- Task 2
 
@@ -17,57 +17,55 @@ dotProduct xs ys = sum (zipWith (*) xs ys)
 
 matMult :: [[Int]] -> [[Int]] -> [[Int]]
 matMult a b = map (\rowA -> map (\colB -> dotProduct rowA colB) (transp b)) a
---Task 3
-pascal :: [Int] -> [Int] 
-pascal list = result
-    where
-        a = 0 : list
-        b = list ++ [0]
-        result = zipWith (+) a b
 
+-- Task 3
+pascal :: [Int] -> [Int]
+pascal list = result
+  where
+    a = 0 : list
+    b = list ++ [0]
+    result = zipWith (+) a b
 
 triangleR :: Int -> [Int]
 triangleR 0 = [1]
-triangleR x = pascal(triangleR(x-1))
+triangleR x = pascal (triangleR (x - 1))
 
 pascalTriangle :: Int -> [[Int]]
 pascalTriangle 0 = [[1]]
-pascalTriangle x =  pascalTriangle(x-1) ++ [triangleR(x)]
+pascalTriangle x = pascalTriangle (x - 1) ++ [triangleR (x)]
 
-
---Task 4
+-- Task 4
 letterNums :: [(Char, Int)]
 letterNums = result
-    where
-        x =  ['A'..'Z'] ++ ['a'..'z'] ++ [' ']
-        result =  zip x [0..]
+  where
+    x = ['A' .. 'Z'] ++ ['a' .. 'z'] ++ [' ']
+    result = zip x [0 ..]
 
 shifter :: [Int] -> Int -> [Int]
-shifter list shift = result 
-    where 
-        result = map (\x -> (x + shift) `mod` 53) list
-
+shifter list shift = result
+  where
+    result = map (\x -> (x + shift) `mod` 53) list
 
 stringToNum :: String -> [Int]
 stringToNum str = [result | letter <- str, Just result <- [lookup letter letterNums]]
 
 numLetters :: [(Int, Char)]
-numLetters = zip [0..] (['A'..'Z'] ++ ['a'..'z'] ++ [' '])
+numLetters = zip [0 ..] (['A' .. 'Z'] ++ ['a' .. 'z'] ++ [' '])
 
 numToString :: [Int] -> String
-numToString list = result 
-    where
-        result = [result | char <- list, Just result <- [lookup char numLetters]]
+numToString list = result
+  where
+    result = [result | char <- list, Just result <- [lookup char numLetters]]
 
 cipher :: String -> Int -> String
-cipher message shift = result 
-    where 
-        a = stringToNum message
-        b = shifter a shift
-        result = numToString b
+cipher message shift = result
+  where
+    a = stringToNum message
+    b = shifter a shift
+    result = numToString b
 
 dicipher message shift = result
-    where
-        a = stringToNum message
-        b = shifter a (-1*shift)
-        result = numToString b
+  where
+    a = stringToNum message
+    b = shifter a (-1 * shift)
+    result = numToString b
